adeeecd53e44835c736c25009e04332ee304a53b
Sam Yin <nw0527@gmail.com>
2017-09-11 14:38:16 -0400

    module import issue fixed

--- nn_utils_tf.py
+++ nn_utils_tf.py
@@ -23,11 +23,8 @@ SOFTWARE.
 rewrite in tensorflow
 """
 import tensorflow as tf
+from tensorflow.contrib.keras import models, layers
 # from keras import backend as K
-from tensorflow.contrib.keras.models import Sequential
-from tensorflow.contrib.keras.layers import (Activation, BatchNormalization,
-                                             Conv1D, Dense, InputLayer,
-                                             Lambda, ZeroPadding1D)
 from layers import PKBiasLayer, PKRowBiasLayer
 
 
@@ -41,19 +38,23 @@ def get_network(batch_size, input_dim, output_dim, hidden_dim, num_layers,
     Also returns a list of PKBias layers
     """
     PKbias_layers = []
-    NN = Sequential()
+    NN = models.Sequential()
     # K.set_learning_phase(0)
-    NN.add(InputLayer(batch_input_shape=(batch_size, input_dim), name="Input"))
+    NN.add(layers.InputLayer(batch_input_shape=(batch_size, input_dim),
+                             name="Input"))
     if filt_size is not None:  # first layer convolution
         # expand dims for convolution
-        NN.add(Lambda(lambda x: tf.expand_dims(x, 0), name="ExpandDims"))
+        NN.add(layers.Lambda(lambda x: tf.expand_dims(x, 0),
+                             name="ExpandDims"))
         # custom pad so that no timepoint gets input from future
-        NN.add(ZeroPadding1D(padding=(filt_size - 1, 0), name="ZeroPadding"))
-        # Perform convolution (no pad)
-        NN.add(Conv1D(filters=hidden_dim, kernel_size=filt_size,
-                      padding='valid', activation=hidden_nonlin, name="Conv"))
+        NN.add(layers.ZeroPadding1D(padding=(filt_size - 1, 0),
+                                    name="ZeroPadding"))
+        # Perform convolution
+        NN.add(layers.Conv1D(filters=hidden_dim, kernel_size=filt_size,
+                             padding='valid', activation=hidden_nonlin,
+                             name="Conv"))
         # squeeze dims for dense layers
-        NN.add(Lambda(lambda x: tf.squeeze(x, [0]), name="Squeeze"))
+        NN.add(layers.Lambda(lambda x: tf.squeeze(x, [0]), name="Squeeze"))
     for i in range(num_layers):
         if is_shooter and add_pklayers:
             if row_sparse:
@@ -70,21 +71,21 @@ def get_network(batch_size, input_dim, output_dim, hidden_dim, num_layers,
             layer_nonlin = hidden_nonlin
 
         if batchnorm and i < num_layers - 1 and i != 0:
-            NN.add(Dense(layer_dim,
-                         name="Dense%s" % i,
-                         kernel_initializer=tf.random_normal_initializer(
-                            stddev=init_std)))
-            NN.add(BatchNormalization(name="BatchNorm%s" % i))
+            NN.add(layers.Dense(
+                layer_dim, name="Dense%s" % i,
+                kernel_initializer=tf.random_normal_initializer(
+                    stddev=init_std)))
+            NN.add(layers.BatchNormalization(name="BatchNorm%s" % i))
             # may set initializer for hyperparams
-            NN.add(Activation(activation=layer_nonlin,
-                              name="Activation%s" % i))
+            NN.add(layers.Activation(activation=layer_nonlin,
+                                     name="Activation%s" % i))
         else:
-            NN.add(Dense(layer_dim,
-                         name="Dense%s" % i,
-                         kernel_initializer=tf.random_normal_initializer(
-                            stddev=init_std)))
-            NN.add(Activation(activation=layer_nonlin,
-                              name="Activation%s" % i))
+            NN.add(layers.Dense(
+                layer_dim, name="Dense%s" % i,
+                kernel_initializer=tf.random_normal_initializer(
+                    stddev=init_std)))
+            NN.add(layers.Activation(activation=layer_nonlin,
+                                     name="Activation%s" % i))
     if add_pklayers:
         return NN, PKbias_layers
     else:
