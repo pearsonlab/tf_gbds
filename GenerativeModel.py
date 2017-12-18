scaling corrected

GenerativeModel.py
@@ -286,6 +286,7 @@ class GBDS_u(RandomVariable, Distribution):
         self.y = y
         self.yDim = yDim
         self.B = tf.shape(y)[0]  # batch size
+        self.Tt = tf.shape(y)[1]
         self.latent_u = GenerativeParams['latent_u']
         self.clip = GenerativeParams['clip']
 
@@ -522,7 +523,7 @@ class GBDS_u(RandomVariable, Distribution):
                 if self.pen_eps is not None:
                     LogDensity -= self.pen_eps * tf.reduce_sum(self.unc_eps)
 
-        return LogDensity
+        return LogDensity / tf.cast(self.Tt, tf.float32)
 
     def getParams(self):
         """Return the learnable parameters of the model
@@ -579,6 +580,7 @@ class GBDS_g(RandomVariable, Distribution):
 
         self.y = y
         self.B = tf.shape(y)[0]  # batch size
+        self.Tt = tf.shape(y)[1]
         self.extra_conds = extra_conds
         self.ctrl_obs = ctrl_obs
 
@@ -792,7 +794,7 @@ class GBDS_g(RandomVariable, Distribution):
             if not self.latent_u:
                 LogDensity += self.u._log_prob(value=None)
 
-        return LogDensity
+        return LogDensity / tf.cast(self.Tt, tf.float32)
 
     def getParams(self):
         """Return the learnable parameters of the model