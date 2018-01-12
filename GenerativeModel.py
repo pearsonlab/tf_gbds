e069e6dbd6fd629ba39deb719cb44c85881afac5
Sam Yin <nw0527@gmail.com>
2018-01-12 11:53:21 -0500

    add clip hinge loss

--- GenerativeModel.py
+++ GenerativeModel.py
@@ -41,7 +41,8 @@ class GBDS_u(RandomVariable, Distribution):
         with tf.name_scope('control_signal_censoring'):
             self.clip_range = GenerativeParams['clip_range']
             self.clip_tol = GenerativeParams['clip_tol']
-            self.eta = GenerativeParams['eta']
+            # self.eta = GenerativeParams['eta']
+            self.clip_pen = GenerativeParams['clip_pen']
 
         with tf.name_scope('control_signal_noise'):
             self.eps = GenerativeParams['PID_params']['eps']
@@ -81,23 +82,30 @@ class GBDS_u(RandomVariable, Distribution):
     def clip_log_prob(self, upsilon, u):
         u_b = self.clip_range - self.clip_tol
         l_b = -self.clip_range + self.clip_tol
-        eta = self.eta
+        # eta = self.eta
+        pen = self.clip_pen
 
-        def z(x, loc, scale):
-            return (x - loc) / scale
+        # def z(x, loc, scale):
+        #     return (x - loc) / scale
 
-        def normal_logpdf(x, loc, scale):
-            return -(0.5 * np.log(2 * np.pi) + tf.log(scale) +
-                     0.5 * tf.square(z(x, loc, scale)))
+        # def normal_logpdf(x, loc, scale):
+        #     return -(0.5 * np.log(2 * np.pi) + tf.log(scale) +
+        #              0.5 * tf.square(z(x, loc, scale)))
 
-        def normal_logcdf(x, loc, scale):
-            return log_ndtr(z(x, loc, scale))
+        # def normal_logcdf(x, loc, scale):
+        #     return log_ndtr(z(x, loc, scale))
 
+        # return tf.where(tf.less_equal(upsilon, l_b),
+        #                 normal_logcdf(-1., u, eta),
+        #                 tf.where(tf.greater_equal(upsilon, u_b),
+        #                          normal_logcdf(-1., -u, eta),
+        #                          normal_logpdf(upsilon, u, eta)))
         return tf.where(tf.less_equal(upsilon, l_b),
-                        normal_logcdf(-1., u, eta),
+                        pen * tf.to_float(u > l_b),
                         tf.where(tf.greater_equal(upsilon, u_b),
-                                 normal_logcdf(-1., -u, eta),
-                                 normal_logpdf(upsilon, u, eta)))
+                                 pen * tf.to_float(u < u_b),
+                                 pen * tf.to_float(
+                                    tf.abs(u - upsilon) > self.clip_tol)))
 
     def _log_prob(self, value):
         with tf.name_scope('next_time_point_pred'):
