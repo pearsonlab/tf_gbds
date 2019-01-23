13acb02586cf03452654f52e629fc7a8746721f5
Sam Yin <nw0527@gmail.com>
2019-01-22 22:50:55 -0500

    Update agents.py

--- agents.py
+++ agents.py
@@ -94,14 +94,14 @@ class game_model(object):
                         a.G0_NN, s, a.K, a.dim, "G0")
 
                     s1 = tf.gather(extra_conds, tf.concat(
-                        [tf.range(self.s_dim), [self.s_dim * 2],
-                         [self.extra_dim - 2]], 0), axis=-1, name="s1")
+                        [tf.range(state_dim), [state_dim * 2],
+                         [extra_dim - 2]], 0), axis=-1, name="s1")
                     G1_mu_1, G1_lambda_1, G1_w_1 = get_GMM_params(
                         a.G1_NN, s1, a.K, a.dim, "G1_1")
 
                     s2 = tf.gather(extra_conds, tf.concat(
-                        [tf.range(self.s_dim, self.s_dim * 2), [self.s_dim * 2 + 1],
-                         [self.extra_dim - 1]], 0), axis=-1, name="s2")
+                        [tf.range(state_dim, state_dim * 2), [state_dim * 2 + 1],
+                         [extra_dim - 1]], 0), axis=-1, name="s2")
                     G1_mu_2, G1_lambda_2, G1_w_2 = get_GMM_params(
                         a.G1_NN, s2, a.K, a.dim, "G1_2")
 
